6708b8c29e87825964ee81c8bc54b29f2df36e84
zXekbrkJH046 <64298658+zXekbrkJH046@users.noreply.github.com>
2021-06-25 19:14:51 +0700

    delete

--- app/src/main/java/com/example/musicapp/Service/APIService.java
+++
@@ -1,8 +0,0 @@
-package com.example.musicapp.Service;
-
-public class APIService {
-    private static String base_url="https://phimrap.net/ws/";
-    public static DataService getService(){
-        return APIRetrofitClient.getClient(base_url).create(DataService.class);
-    }
-}
